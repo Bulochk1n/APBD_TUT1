e3ae6efacd0635bcdc7b8ee1df11528bd23ec03a
Danila Novik <s30917@pjwstk.edu.pl>
2025-03-03 11:15:55 +0100

    Third modification.

--- task2/task2/Program.cs
+++ task2/task2/Program.cs
@@ -4,5 +4,6 @@ Console.WriteLine("This is the first modification");
 
 int a = 8;
 int b = 10;
-Console.WriteLine("This is the second modification: " + a + b); 
+Console.WriteLine("This is the second modification: " + a + b);
 
+Console.WriteLine("This is the second modification.");
