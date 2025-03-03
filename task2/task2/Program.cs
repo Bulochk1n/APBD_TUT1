867bd7788868a01ff5918a72ee371a15ce38d6a4
Danila Novik <s30917@pjwstk.edu.pl>
2025-03-03 11:14:56 +0100

    Scond modification

--- task2/task2/Program.cs
+++ task2/task2/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("This is the first modification");
+
+int a = 8;
+int b = 10;
+Console.WriteLine("This is the second modification: " + a + b); 
+
