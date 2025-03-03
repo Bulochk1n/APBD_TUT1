9b8b9d8b310419806bbe7678eca6801625e299a0
Danila Novik <s30917@pjwstk.edu.pl>
2025-03-03 11:31:12 +0100

    new-feature

--- task2/task2/Program.cs
+++ task2/task2/Program.cs
@@ -7,3 +7,18 @@ int b = 10;
 Console.WriteLine("This is the second modification: " + a + b);
 
 Console.WriteLine("This is the second modification.");
+
+int[] arr = [3,5,2,1];
+Console.WriteLine("Array length: " + arr.Length + ", array sum: " + arr.Sum());
+
+static float feature(int[] arr)
+{
+    int sum = 0;
+        
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum = sum + arr[i];
+    }
+    return arr.Sum() / arr.Length;
+}
+
